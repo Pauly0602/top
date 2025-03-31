ab15037e978be44f8ba3f0307c8052434b703fa7
pauly0602 <paulina.woernhoer@student.uibk.ac.at>
2025-03-31 18:39:30 +0200

    Pulldown befüllt

--- main.js
+++ main.js
@@ -8,101 +8,197 @@ let stop = {
   lat: 37.77,
   lng: -25.46,
   zoom: 11,
-};
-const STOPS = [
-  {
-    nr: 1,
-    user: "vintiyannick",
-},
+}
+
+  const STOPS = [
+    {
+        lat: 45.109544,
+        lng: 7.641281,
+        zoom: 13,
+        title: "Juventus Stadium",
+        nr: 1,
+        user: "vintiyannick",
+    },
+    {
+        nr: 2,
+        title: "Schrammsteinaussicht",
+        user: "Ellinnaa",
+        lat: 50.914167,
+        lng: 14.203333,
+        zoom: 15,
+    },
+    {
+        nr: 3,
+        title: "Boston",
+        user: "samuesl",
+        lat: 42.35,
+        lng: -71.05,
+        zoom: 13,
+    },
+    {
+        nr: 4,
+        title: "Grevelingen",
+        user: "Gregorysprenger2001",
+        lat: 51.755833,
+        lng: 3.8925,
+        zoom: 11,
+    },
+    {
+        nr: 5,
+        title: "Rotorua",
+        user: "lukas6020",
+        lat: -38.14,
+        lng: 176.25,
+        zoom: 13,
+    },
+    {
+        nr: 6,
+        title: "Vancouver",
+        user: "johannauniibk",
+        lat: 49.28098,
+        lng: -123.12244,
+        zoom: 11,
+    },
+    {
+        nr: 7,
+        title: "Istanbul",
+        user: "sam-uze",
+        lat: 41.01,
+        lng: 28.96,
+        zoom: 13,
+    },
+    {
+        nr: 8,
+        title: "La Push",
+        user: "webmapping",
+        lat: 47.908683,
+        lng: -124.636604,
+        zoom: 13,
+    },
 {
-    nr: 2,
-    user: "Ellinnaa",
-},
-{
-    nr: 3,
-    user: "samuesl",
-},
- {  
-    nr: 4,
-    user:"Gregorysprenger2001",
-},
-{
-    nr: 5,
-    user: "lukas6020",
-},
-{
-    nr: 6,
-    user: "johannauniibk",
-},
-{
-    nr: 7,
-    user: "sam-uze",
-},
-{
-    nr: 8,
-    user: "webmapping",
-},
-{
+
     nr: 9,
+    title: "Konstanz",
     user: "cs4151",
+    lat: 47.6633,
+    lng: 9.175,
+    zoom: 14,
 },
-{
+    {
+       title: "Azoren",
   user: "pauly0602",
   nr: 10,
-  },
-{  
-    nr: 11,
-    user: "lujehle",
-},
-{
-    nr: 12,
-    user: "benmertens",
-},
-{
-    nr: 13,
-    user: "johaschra",
-},
-{
-    nr: 16,
-    user: "lizzie2911",
-},
-{
-  nr: 17,
-  user: "Kathleenuniibk",
-},
-{
-    nr: 18,
+  lat: 37.77,
+  lng: -25.46,
+  zoom: 11,
+    },
+    {
+        nr: 11,
+        title: "Nelson",
+        user: "lujehle",
+        lat: -41.270833,
+        lng: 173.284167,
+        zoom: 11,
+    },
+    {
+        nr: 12,
+        title: "Axamer Lizum",
+        user: "benmertens",
+        lat: 47.193889,
+        lng: 11.302222,
+        zoom: 11,
+    },
+    {
+        nr: 13,
+        title: "Pancake rocks (Punakaiki)",
+        user: "johaschra",
+        lat: -42.114383,
+        lng: 171.327320,
+        zoom: 11,
+    },
+    {
+        nr: 16,
+        title: "Provence",
+        user: "lizzie2911",
+        lat: 43.5,
+        lng: 5.5,
+        zoom: 13,
+    },
+    {
+        nr: 17,
+        title: "Ulriken",
+        user: "Kathleenuniibk",
+        lat: 60.383333,
+        lng: 5.383333,
+        zoom: 13,
+    },
+
+    {nr: 18,
+    title: "Volksparkstadion",
     user: "Pruje839",
-},
-{
-    nr: 20,
-    user: "florentinebusch",
-},
-{
-    nr: 21,
-    user: "moplatt",
-},
+    lat: 53.587153,
+    lng: 9.898643,
+    zoom: 12,
+    },
+    {
+        nr: 20,
+        title: "Doubtful Sound",
+        user: "florentinebusch",
+        lat: -45.317222,
+        lng: 166.988333,
+        zoom: 11,
+    },
+    {
+nr: 21,
+title: "Tafraoute",
+user: "moplatt",
+lat: 29.72222,
+lng: -8.97194,
+zoom: 11,
+    },
 {
     nr: 23,
+    title: "Habicht",
     user: "fritzcrone",
+    lat: 47.043611,
+    lng: 11.289444,
+    zoom: 15
 },
-{
-    nr: 24,
+    {
+        nr: 24,
+    title: "Sevilla",
     user: "StephanPumpernik",
-},
-{
-    nr: 25,
-    user: "PriPh625",
-},
-{
-    nr: 26,
-    user: "Basti-10",
-},
-{
-    nr: 29,
+    lat: 37.3925,
+    lng: -5.9925,
+    zoom: 13,
+    },
+    {
+        lat: 49.577037,
+        lng: 6.114122,
+        zoom: 11,
+        title: "Stade de Luxembourg",
+        nr: 25,
+        user: "PriPh625",
+    },
+    {
+        title: "Kalmit",
+        nr: 26,
+        user: "Basti-10",
+        zoom: 13,
+        lat: 49.31,
+        lng: 8.08,
+    },
+    {
+        
+nr: 29,
+    title: 'Utrecht',
     user: 'jessimeteo',
-},
-]
+    lat: 52.088889,
+    lng: 5.115556,
+    zoom: 13
+    },
+];
+
 // Karte initialisieren
 let map = L.map('map')
 // Hintergrund definieren 
